2826de8663a3055fa41eb3efaef6d773d475eb82
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-15 11:24:17 +0100

    Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-	let { children } = $props();
+	let { children } = $props()
+	const menu = [
+		{ label: 'Kezdőoldal', href: '/' },
+		{ label: 'Ki- és bejelentkezés', href: '/login' }
+	]
 </script>
-cica
+<div>
+{#each menu as item}
+	<a class="ui small green button" href={item.href}>{item.label}</a>
+{/each}
+</div>
 {@render children()}
