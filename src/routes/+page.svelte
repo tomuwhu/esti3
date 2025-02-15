e36d2242cd96a3da666b109607aaa5c143ce7faa
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-15 11:07:31 +0100

    Kezdőlap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="ui header">Iskolai könyvtár</h1>
+<h3 class="ui header">Kezdőlap</h3>
+<img src="https://www.creativefabrica.com/wp-content/uploads/2022/03/09/Librarian-SVG-School-Librarian-SVG-Graphics-26751737-1-1-580x348.jpg" alt="">
+<style>
+    img {
+        box-shadow: 1px 1px 7px black;
+    }
+</style>
