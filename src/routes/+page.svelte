a68cd7098101d40553f7ba52ac82590bb2edfdb1
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-16 15:22:00 +0100

    Kezdőoldal kép cseréje

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <h1 class="ui header">Iskolai könyvtár</h1>
 <h3 class="ui header">Kezdőlap</h3>
-<img src="https://www.creativefabrica.com/wp-content/uploads/2022/03/09/Librarian-SVG-School-Librarian-SVG-Graphics-26751737-1-1-580x348.jpg" alt="">
+<img src="https://d4804za1f1gw.cloudfront.net/wp-content/uploads/sites/50/2018/11/hero.jpg" alt="">
 <style>
     img {
         box-shadow: 1px 1px 7px black;
+        width: 900px;
+        border-radius: 20px;
     }
 </style>
