11548226cfa77dba73cd73f7f055c80891bd4ae0
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-17 08:20:10 +0100

    login/logout menüpont külön szöveg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,21 @@
 	import { page } from '$app/stores'
 	import { afterNavigate } from '$app/navigation'
 	var path = $state($page.url.pathname)
-	let { children } = $props()
-	const menu = [
+	let { children, data } = $props()
+	var user = $state(data.user)
+	const menu = $derived([
 		{ label: 'Kezdőoldal', href: '/' },
 		{ label: 'Könyvek', href: '/konyvek' },
-		{ label: 'Ki- és bejelentkezés', href: '/login' }
-	]
+		{ label: user?.id ? 'Kijelentkezés' : 'Regisztráció / Bejelentkezés', href: '/login' },
+	])
 	afterNavigate(() => {
 		path = $page.url.pathname
+		user = data.user
   	})
 </script>
 <div>
 {#each menu as item}
-    {#if item.href === path}
+    {#if item.href === path || item.href + '/login' === path}
 		<a class="ui small blue button" href={item.href}>{item.label}</a>
 	{:else}
 		<a class="ui small button" href={item.href}>{item.label}</a>
